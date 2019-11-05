73ef5c222a54e3bdeeafeb23766095dba7e68fff
Ubuntu <ubuntu@ip-172-31-83-93.ec2.internal>
2019-11-05 01:03:04 +0000

    deploy

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -34,8 +34,6 @@ getPage1R = do
         toWidgetHead $(juliusFile "templates/page2.julius")
         addScript (StaticR ola_js)
         [whamlet|
-        
-        
             <h1> Página 1
             
             <a href=@{HomeR}>
@@ -51,38 +49,28 @@ getHomeR = do
     defaultLayout $ do
         addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"
         addScriptRemote "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        -- addScriptRemote "url" -> js cdn
         -- addScript (Static script_js) -> js interno
         -- 8aQZvtkO
         $(whamletFile "templates/header.hamlet")
         toWidgetHead $(luciusFile "templates/page2.lucius")
         toWidgetHead $(juliusFile "templates/page2.julius")
-        -- addStylesheet (StaticR css_bootstrap_css)
+        addStylesheet (StaticR bootstrap_css)
         toWidgetHead [julius|
-            function ola(){
-                alert("ola mundo");
-            }
+            -- function ola(){
+            --     alert("ola mundo");
+            -- }
             
         |]
         toWidgetHead [lucius|
-            h1{
-                color: red;
-            }
+            -- h1{
+            --     color: red;
+            -- }
         |]
         [whamlet|
-            <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-            <h1>Olá mundovbnhjjmn
-            <ul>
-                <li>
-                    <a href=@{Page1R}>Página 1
-                    
-                <li>
-                    <a href=@{Page2R}>Página 2
-                    
-                <li>
-                    <a href=@{Page3R}>Página 3
+            -- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <h1>Olá mundo
             
             <img src=@{StaticR santos_png}>
             
-            <button onclick="ola()" class="btn btn-danger">Clickar
+            -- <button onclick="ola()" class="btn btn-danger">Clickar
         |]
