7bc8d71189a5678e52133da91098d12df5c088a7
Ubuntu <ubuntu@ip-172-31-83-93.ec2.internal>
2019-11-05 01:04:30 +0000

    deploy

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -54,7 +54,7 @@ getHomeR = do
         $(whamletFile "templates/header.hamlet")
         toWidgetHead $(luciusFile "templates/page2.lucius")
         toWidgetHead $(juliusFile "templates/page2.julius")
-        addStylesheet (StaticR bootstrap_css)
+        addStylesheet (StaticR css_bootstrap_css)
         toWidgetHead [julius|
             function ola(){
                 alert("ola mundo");
