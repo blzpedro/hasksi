9c0d04efcd4efd015e896c5281ab8a6665af0e41
Ubuntu <ubuntu@ip-172-31-83-93.ec2.internal>
2019-11-06 00:38:06 +0000

    deploy

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -20,7 +20,6 @@ getContatoR = do
         addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"
         addScriptRemote "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         $(whamletFile "templates/nav-header.hamlet")
-        $(whamletFile "templates/header.hamlet")
         toWidgetHead $(luciusFile "templates/adCSS.lucius")
         toWidgetHead $(juliusFile "templates/adScript.julius")
         addStylesheet (StaticR css_bootstrap_css)
@@ -31,7 +30,6 @@ getGaleriaR = do
         addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"
         addScriptRemote "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         $(whamletFile "templates/nav-header.hamlet")
-        $(whamletFile "templates/header.hamlet")
         toWidgetHead $(luciusFile "templates/adCSS.lucius")
         toWidgetHead $(juliusFile "templates/adScript.julius")
         addStylesheet (StaticR css_bootstrap_css)
@@ -50,6 +48,7 @@ getHomeR = do
         -- 8aQZvtkO
         $(whamletFile "templates/nav-header.hamlet")
         $(whamletFile "templates/header.hamlet")
+        $(whamletFile "templates/body-home.hamlet")
         toWidgetHead $(luciusFile "templates/adCSS.lucius")
         toWidgetHead $(juliusFile "templates/adScript.julius")
         addStylesheet (StaticR css_bootstrap_css)
