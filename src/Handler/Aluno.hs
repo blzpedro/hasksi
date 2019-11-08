83d175f94228f58af15ce7bbd5d1da5b32b84603
Ubuntu <ubuntu@ip-172-31-83-93.ec2.internal>
2019-11-08 00:41:09 +0000

    deploy

--- src/Handler/Aluno.hs
+++ src/Handler/Aluno.hs
@@ -1,39 +1,36 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 module Handler.Aluno where
 
 import Import
-import Data.FileEmbed(embedFile)
 import Text.Lucius
 import Text.Julius
---import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
+-- renderDivs
+formAluno :: Form Aluno 
+formAluno = renderBootstrap $ Aluno 
+    <$> areq textField "Nome: " Nothing 
+    <*> areq textField "RA: "   Nothing 
+    <*> areq dayField  "Data: " Nothing 
 
-formAluno :: Form Aluno
-formAluno = renderBootstrap $ Aluno
-    <$> areq textField "Nome: " Nothing
-    <*> areq textField "RA: " Nothing
-    <*> areq dayField "Data: " Nothing
-
-
-getAlunoR :: Handler Html
-getAlunoR = do
-    (widget, enctype) <- generateFormPost formAluno
+getAlunoR :: Handler Html 
+getAlunoR = do 
+    (widget, enctype) <- generateFormPost formAluno 
     msg <- getMessage
-    defaultLayout $ do
-    addSylesheet (StaticR css_bootstrap_css)
-        [whamlet |
-              $maybe mensa < - msg
-              <div>
+    defaultLayout $ do 
+        addStylesheet (StaticR css_bootstrap_css)
+        [whamlet|
+            $maybe mensa <- msg
+                <div>
                     ^{mensa}
-             
+            
             <h1>
-            CADASTRO DE ALUNO 
+                CADASTRO DE ALUNO
             
             <form method=post action=@{AlunoR}>
                 ^{widget}
@@ -41,31 +38,28 @@ getAlunoR = do
                     Cadastrar
         |]
 
+postAlunoR :: Handler Html 
+postAlunoR = do 
+    ((result,_),_) <- runFormPost formAluno 
+    case result of 
+        FormSuccess aluno -> do 
+            runDB $ insert aluno 
+            setMessage [shamlet|
+                <h2>
+                    REGISTRO INCLUIDO
+            |]
+            redirect AlunoR
+        _ -> redirect HomeR
 
-getListAlunoR :: Handler Html
-getListAlunoR = do
-    --select * from aluno order by aluno.nome
-        alunos <- runDB $ selectList [] [Asc AlunoNome]
-        defaultLayout $ do 
+getListAlunoR :: Handler Html 
+getListAlunoR = do 
+    -- select * from aluno order by aluno.nome
+    alunos <- runDB $ selectList [] [Asc AlunoNome]
+    defaultLayout $ do 
         $(whamletFile "templates/alunos.hamlet")
 
-
-
-postAlunoR :: Handler Html
-postAlunoR = do
-    ((result,_),_) <- runFormPost formAluno
-    case result of
-        FormSucess aluno -> do
-        runDB $ insert aluno
-        setMessage [shamlet|
-            <h2>
-                Registro Incluido
-        ]
-        redirect AlunoR    
-    _ -> redirect HomeR
-
-postApagarAlunoR :: AlunoId -> Handler Html
-postApagarAlunoR aid = do
+postApagarAlunoR :: AlunoId -> Handler Html 
+postApagarAlunoR aid = do 
     _ <- runDB $ get404 aid
-    runDB $ delete aid
-    redirect ListarAlunoR
+    runDB $ delete aid 
+    redirect ListAlunoR
