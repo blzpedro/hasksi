99d235e6837fe6ae4bfd67a9fd9999686de9b357
Ubuntu <ubuntu@ip-172-31-83-93.ec2.internal>
2019-11-08 00:02:22 +0000

    deploy'

---
+++ src/Handler/Aluno.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Aluno where
+
+import Import
+import Data.FileEmbed(embedFile)
+import Text.Lucius
+import Text.Julius
+--import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+
+
+formAluno :: Form Aluno
+formAluno = renderBootstrap $ Aluno
+    <$> areq textField "Nome: " Nothing
+    <*> areq textField "RA: " Nothing
+    <*> areq dayField "Data: " Nothing
+
+
+getAlunoR :: Handler Html
+getAlunoR = do
+    (widget, enctype) <- generateFormPost formAluno
+    msg <- getMessage
+    defaultLayout $ do
+    addSylesheet (StaticR css_bootstrap_css)
+        [whamlet |
+            $maybe mensa <- msg
+                <div>
+                    ^{mensa}
+                    
+            <h1>
+            CADASTRO DE ALUNO 
+            
+            <form method=post action-@{AlunoR}>
+                ^{widget}
+                <button>
+                    Cadastrar
+        |]
+
+postAlunoR :: Handler Html
+postAlunoR = do
+    ((result,_),_) <- runFormPost formAluno
+    case result of
+        FormSucess aluno -> do
+        runDB $ insert aluno
+        setMessage [shamlet|
+            <h2>
+                Registro Incluido
+        ]
+        redirect AlunoR    
+    _ -> redirect HomeR
+
+
