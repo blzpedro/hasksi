93ee50f1c499ea600aacd6f5edf45340d075e5ee
Ubuntu <ubuntu@ip-172-31-83-93.ec2.internal>
2019-12-05 17:28:59 +0000

    deploy

--- src/Handler/Login.hs
+++ src/Handler/Login.hs
@@ -75,7 +75,6 @@ getAdminR = do
         setTitle "HaskellMilGrau"
         $(whamletFile "templates/header.hamlet")
         $(whamletFile "templates/home.hamlet")
-        toWidget $(juliusFile "templates/home.julius")
         toWidgetHead [hamlet|
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
               <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
