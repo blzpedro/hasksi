556d7cd93eee6f616085f59e73b58f5bc9e85719
Ubuntu <ubuntu@ip-172-31-83-93.ec2.internal>
2019-12-05 17:47:11 +0000

    deploy

--- src/Handler/Usuario.hs
+++ src/Handler/Usuario.hs
@@ -32,6 +32,7 @@ getUsuarioR = do
         addStylesheet (StaticR css_main_css)
         $(whamletFile "templates/usuario.hamlet")
         $(whamletFile "templates/footer.hamlet")
+        addScriptRemote "https://code.jquery.com/jquery-3.4.1.min.js"
         addScriptRemote "https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"
         
 postUsuarioR :: Handler Html
